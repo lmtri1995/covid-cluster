8d605d422240732ea815d13dc66c6be546a6993f
Tri Le <lmtri1995@gmail.com>
2020-07-09 22:07:18 +0700

    Draw Hexagrid based on coordinate

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { GridGenerator, HexGrid, Layout, Path, Hexagon, Text, Pattern, Hex } from 'react-hexgrid';
+import HexaGrid from '../src/components/HexaGrid'
 
-
+const hexaGridData = {'0,0': 'ax', '0,-1': 'bx', '0,1': 'cx', '1,-1': 'dx', '1,0': 'ex', '-1,1': 'fx', '-1,0': 'gx', '-2,0': 'hx'}
 function App() {
-  const hexagonSize = { x: 10, y: 10 };
-  const moreHexas = GridGenerator.parallelogram(-2, 2, -2, 2);
   return (
     <div className="App">
       <h2>Welcome to Hexaland</h2>
-      <HexGrid width={1200} height={800} viewBox="-50 -50 100 100">
-        {/* Main grid with bit hexagons, all manual */}
-        <Layout size={hexagonSize} flat={true} spacing={1.1} origin={{ x: 0, y: 0 }}>
-          <Hexagon q={0} r={0} s={0} />
-          {/* Using pattern (defined below) to fill the hexagon */}
-          <Hexagon q={0} r={-1} s={1} fill="pat-1" />
-          <Hexagon q={0} r={1} s={-1} />
-          <Hexagon q={1} r={-1} s={0}>
-            <Text>1, -1, 0</Text>
-          </Hexagon>
-          <Hexagon q={1} r={0} s={-1}>
-            <Text>1, 0, -1</Text>
-          </Hexagon>
-          {/* Pattern and text */}
-          <Hexagon q={-1} r={1} s={0} fill="pat-2">
-            <Text>-1, 1, 0</Text>
-          </Hexagon>
-          <Hexagon q={-1} r={0} s={1} />
-          <Hexagon q={-2} r={0} s={1} />
-        </Layout>
-      </HexGrid>
+      <HexaGrid data={hexaGridData} />
     </div>
   );
 }
