60cfeb9075a39eb7f59a836e57ccc01d0cf01cb1
Tri Le <lmtri1995@gmail.com>
2020-07-09 21:28:57 +0700

    Add react-hexgrid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GridGenerator, HexGrid, Layout, Path, Hexagon, Text, Pattern, Hex } from 'react-hexgrid';
+
 
 function App() {
+  const hexagonSize = { x: 10, y: 10 };
+  const moreHexas = GridGenerator.parallelogram(-2, 2, -2, 2);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome to Hexaland</h2>
+      <HexGrid width={1200} height={800} viewBox="-50 -50 100 100">
+        {/* Main grid with bit hexagons, all manual */}
+        <Layout size={hexagonSize} flat={true} spacing={1.1} origin={{ x: 0, y: 0 }}>
+          <Hexagon q={0} r={0} s={0} />
+          {/* Using pattern (defined below) to fill the hexagon */}
+          <Hexagon q={0} r={-1} s={1} fill="pat-1" />
+          <Hexagon q={0} r={1} s={-1} />
+          <Hexagon q={1} r={-1} s={0}>
+            <Text>1, -1, 0</Text>
+          </Hexagon>
+          <Hexagon q={1} r={0} s={-1}>
+            <Text>1, 0, -1</Text>
+          </Hexagon>
+          {/* Pattern and text */}
+          <Hexagon q={-1} r={1} s={0} fill="pat-2">
+            <Text>-1, 1, 0</Text>
+          </Hexagon>
+          <Hexagon q={-1} r={0} s={1} />
+          <Hexagon q={-2} r={0} s={1} />
+        </Layout>
+      </HexGrid>
     </div>
   );
 }
