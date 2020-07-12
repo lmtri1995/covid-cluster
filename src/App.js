e65eda7c4f1774956bf5d05b0b32238a02abf45e
Tri Le <lmtri1995@gmail.com>
2020-07-12 17:42:40 +0700

    remove context api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { useState } from 'react'
 import './App.css'
 import Layout from '../src/views/Layout'
-import { MainProvider } from './contexts/MainContext'
 
 function App() {
-  
+
 
   return (
     <div className="App">
-      <MainProvider>
-        <Layout />
-      </MainProvider>
+      <Layout />
     </div>
   );
 }
