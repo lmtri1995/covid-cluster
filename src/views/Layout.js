75df0a657d56716ae62ae83c01fa4ccc4c294f80
Tri Le <lmtri1995@gmail.com>
2020-07-12 15:07:47 +0700

    add loading

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -27,6 +27,7 @@ function Layout() {
   const [hexaGridData, setGridData] = useState([])
   const [message, setMessage] = useState('')
   const [notificationColor, setNotificationColor] = useState('')
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
     apiClient.api.getAllHexa().then((response) => {
@@ -42,9 +43,12 @@ function Layout() {
     apiClient.api.searchHexa(name).then((response) => {
       console.log(response)
       setNotificationColor('#edfcef')
+      setLoading(false)
     }).catch((error) => {
+      console.log(error.response.data.message)
       setMessage(error.response.data.message)
       setNotificationColor('#f7646475')
+      setLoading(false)
     })
   }
 
@@ -56,23 +60,28 @@ function Layout() {
       setGridData(removed)
       setMessage(`You 've successfully removed hexagon ${name}`)
       setNotificationColor('#c9f3ce')
+      setLoading(false)
     }).catch((error) => {
       setMessage(error.response.data.message)
       setNotificationColor('#f7646475')
+      setLoading(false)
     })
   }
 
   const handleAddHexa = (hexa) => {
+    setLoading(true)
     apiClient.api.addHexa(hexa).then((response) => {
       
       const newHexa = response.data
       setGridData([...hexaGridData, newHexa])
       setMessage(`You 've successfully added hexagon ${newHexa.name}`)
       setNotificationColor('#c9f3ce')
+      setLoading(false)
     })
   }
 
   const handleSearchRemoveHexa = (hexa) => {
+    setLoading(true)
     if(hexa.action === 'search') {
       searchHexa(hexa.name)
     } else if(hexa.action === 'remove') {
@@ -91,13 +100,20 @@ function Layout() {
     )
   }
 
+  let notification
+  if(isLoading) {
+    notification = <CircularProgress />
+  } else {
+    notification = <Notification handleMessage={closeNotification} message={message} color={notificationColor}/>
+  }
+
   const isGridEmpty = (Object.keys(hexaGridData).length === 0) ? true : false
   return (
     <>
       <h2>Welcome to Hexaland</h2>
       {hexaGrid}
       <Container>
-        <Notification handleMessage={closeNotification} message={message} color={notificationColor}/>
+        {notification}
         <section style={{ justifyContent: 'center', display: 'flex' }} >
           <div className={classes.addHexa}>
             <AddHexa onChange={handleAddHexa} isGridEmpty={isGridEmpty}/>
