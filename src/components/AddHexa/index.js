303557d7e3dcd1ba95596484e45d5bcd154e0451
Tri Le <lmtri1995@gmail.com>
2020-07-13 07:23:24 +0700

    reformat code

--- src/components/AddHexa/index.js
+++ src/components/AddHexa/index.js
@@ -1,12 +1,12 @@
-import React, { useState, useRef, useEffect } from 'react'
-import FormControl from '@material-ui/core/FormControl'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core/styles'
-import Select from '@material-ui/core/Select'
-import MenuItem from '@material-ui/core/MenuItem'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import FormLabel from '@material-ui/core/FormLabel'
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import React, { useRef, useState } from 'react';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -122,7 +122,7 @@ function AddHexa(props) {
   return (
     <FormControl className={classes.root}>
       <FormLabel>Add new hexagon</FormLabel>
-		  <TextField
+      <TextField
         error={!nameValid}
         required
         margin="dense"
